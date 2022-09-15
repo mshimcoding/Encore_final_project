fe8fa33f49aeba61103803e5d69478f7efeb6898
mshimcoding <105092411+mshimcoding@users.noreply.github.com>
2022-09-15 17:24:44 +0900

    Update send_email.py

--- Final_Project/send_email.py
+++ Final_Project/send_email.py
@@ -2,24 +2,25 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-mail_content = '''Hello,
-This is a simple mail. There is only text, no attachments are there The mail is sent using Python SMTP library.
-Thank You'''
+mail_content = '''WARNING TEST WARNING TEST WARNING TEST WARNING TEST WARNING TEST'''
 
-#The mail addresses and password
-sender_address = 'hojun1105@gmail.com'
-sender_pass = 'hrljmicrfodkcpgg'
-receiver_address = 'hojun1105@gmail.com'
-#Setup the MIME
-message = MIMEMultipart()
-message['From'] = sender_address
-message['To'] = receiver_address
-message['Subject'] = 'A test mail sent by Python. It has an attachment.'   #The subject line
-#The body and the attachments for the mail
-message.attach(MIMEText(mail_content, 'plain'))
-#Create SMTP session for sending the mail
-session = smtplib.SMTP('smtp.gmail.com', 587) #use gmail with port
-session.starttls() #enable security
-session.login(sender_address, sender_pass) #login with mail_id and password
-text = message.as_string()
-session.sendmail(sender_address, receiver_address, text)
+def email_warning(id):
+    #The mail addresses and password
+    sender_address = 'hojun1105@gmail.com'
+    sender_pass = 'hrljmicrfodkcpgg'
+    receiver_address = id
+    #Setup the MIME
+    message = MIMEMultipart()
+    message['From'] = sender_address
+    message['To'] = receiver_address
+    message['Subject'] = 'A test WARNING mail sent by Python. It has an attachment.'   #The subject line
+    #The body and the attachments for the mail
+    message.attach(MIMEText(mail_content, 'plain'))
+    #Create SMTP session for sending the mail
+    session = smtplib.SMTP('smtp.gmail.com', 587) #use gmail with port
+    session.starttls() #enable security
+    session.login(sender_address, sender_pass) #login with mail_id and password
+    text = message.as_string()
+    session.sendmail(sender_address, receiver_address, text)
+
+    print("warning sent to" + id)
